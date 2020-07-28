12fe0c680304d0474b03a72bdd2b1056a3aabc82
Flynn Calcutt <flynn.calcutt@hotmail.com>
2020-07-28 21:19:17 +1000

    Moved dream refill hook logic into dream refill file in a new class

---
+++ Entities/SyncedZipMoverActivationController.cs
@@ -0,0 +1,43 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.CommunalHelper {
+    [CustomEntity("CommunalHelper/SyncedZipMoverActivationController")]
+    class SyncedZipMoverActivationController : Entity {
+		private Level level;
+
+		private string colorCode; 
+		private float resetTimer = 0f;
+		private  float resetTime;
+
+		public SyncedZipMoverActivationController(Vector2 position, string colorCode, float zipMoverSpeedMult)
+			: base(position) {
+			this.colorCode = colorCode;
+			this.resetTime = 0.6f + 0.5f / zipMoverSpeedMult;
+		}
+
+		public SyncedZipMoverActivationController(EntityData data, Vector2 offset)
+			: this(data.Position + offset, data.Attr("colorCode", "000000"), data.Float("zipMoverSpeedMultiplier", 1)) {
+		}
+
+		public override void Added(Scene scene) {
+			base.Added(scene);
+			level = scene as Level;
+		}
+
+		public override void Removed(Scene scene) {
+			base.Removed(scene);
+		}
+
+		public override void Update() {
+			base.Update();
+			if (resetTimer > 0) {
+				resetTimer -= Engine.DeltaTime;
+			} else if (Input.Grab.Pressed) {
+				level.Session.SetFlag($"ZipMoverSync:{colorCode}");
+				resetTimer = resetTime;
+			}
+		}
+	}
+}
