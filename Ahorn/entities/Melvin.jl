f3244ba15cb2e5db77eba2ca94fe3671d4fb53b1
caaterpillar <espritox@gmail.com>
2020-08-19 20:42:13 +0200

    Updated Melvin Ahorn plugin to render its tiles randomly.

--- Ahorn/entities/Melvin.jl
+++ Ahorn/entities/Melvin.jl
@@ -22,13 +22,33 @@ const block = "objects/CommunalHelper/melvin/block"
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::Melvin, room::Maple.Room)
     width = Int(get(entity.data, "width", 24))
     height = Int(get(entity.data, "height", 24))
+    
+    tileRands = Ahorn.getDrawableRoom(Ahorn.loadedState.map, room).fgTileStates.rands
+
+    x, y = Ahorn.position(entity)
+    rtx = floor(Int, x / 8)
+    rty = floor(Int, y / 8)
 
     tileWidth = ceil(Int, width / 8)
     tileHeight = ceil(Int, height / 8)
 
     for i in 1:tileWidth, j in 1:tileHeight
         tx = (i == 1) ? 0 : ((i == tileWidth) ? 24 : 8)
-        ty = (j == 1) ? 0 : ((j == tileHeight) ? 24 : 16)
+        ty = (j == 1) ? 0 : ((j == tileHeight) ? 24 : 8)
+        
+        tileChoice = mod(get(tileRands, (rty + j, rtx + i), 0), 4)
+        
+        if tx == 8
+            if tileChoice == 1 || tileChoice == 3
+                tx += 8
+            end
+        end
+        if ty == 8
+            if tileChoice == 2 || tileChoice == 3
+                ty += 8
+            end
+        end
+        
 
         Ahorn.drawImage(ctx, block, (i - 1) * 8, (j - 1) * 8, tx, ty, 8, 8)
     end
