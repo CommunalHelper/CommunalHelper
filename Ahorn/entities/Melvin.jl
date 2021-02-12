2d1562d2da62c64baf3c409b157d695062cbce80
catapillie <espritox@gmail.com>
2021-02-12 17:52:26 +0100

    Re-added Melvin entity and got tile rendering correct

---
+++ Ahorn/entities/Melvin.jl
@@ -0,0 +1,82 @@
+module CommunalHelperMelvin
+using ..Ahorn, Maple
+
+@mapdef Entity "CommunalHelper/Melvin" Melvin(
+			x::Integer, y::Integer,
+            width::Integer=24, height::Integer=24,
+            axes::String = "none")
+           
+const placements = Ahorn.PlacementDict(
+    "Melvin ($(uppercasefirst(axes))) (Communal Helper)" => Ahorn.EntityPlacement(
+        Melvin,
+        "rectangle",
+        Dict{String, Any}(
+            "axes" => axes
+        )
+    ) for axes in Maple.kevin_axes
+)
+
+Ahorn.minimumSize(entity::Melvin) = 24, 24
+Ahorn.resizable(entity::Melvin) = true, true
+
+Ahorn.selection(entity::Melvin) = Ahorn.getEntityRectangle(entity)
+
+Ahorn.editingOptions(entity::Melvin) = Dict{String, Any}(
+    "axes" => Maple.kevin_axes
+)
+
+const edges = Dict{String, String}(
+    "none" => "objects/CommunalHelper/melvin/surf_A",
+    "horizontal" => "objects/CommunalHelper/melvin/surf_H",
+    "vertical" => "objects/CommunalHelper/melvin/surf_V",
+    "both" => "objects/CommunalHelper/melvin/surf_A"
+)
+const insideTiles = "objects/CommunalHelper/melvin/inside"
+const eye = "objects/CommunalHelper/melvin/eye/idle_small00"
+
+function getBlockTextures(axes::String)
+    return insideTiles, edges[axes]
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::Melvin, room::Maple.Room)
+    width = Int(get(entity.data, "width", 24))
+    height = Int(get(entity.data, "height", 24))
+    
+    inside, block = getBlockTextures(lowercase(get(entity.data, "axes", "none")))
+    eyeSprite = Ahorn.getSprite(eye, "Gameplay")
+
+    tileRands = Ahorn.getDrawableRoom(Ahorn.loadedState.map, room).fgTileStates.rands
+
+    x, y = Ahorn.position(entity)
+    rtx = floor(Int, x / 8)
+    rty = floor(Int, y / 8)
+
+    tileWidth = ceil(Int, width / 8)
+    tileHeight = ceil(Int, height / 8)
+
+    for i in 1:tileWidth, j in 1:tileHeight
+        tx = (i == 1) ? 0 : ((i == tileWidth) ? 24 : 8)
+        ty = (j == 1) ? 0 : ((j == tileHeight) ? 24 : 8)
+        isEdge = (i == 1 || i == tileWidth || j == 1 || j == tileHeight)
+        tileChoice = mod(get(tileRands, (rty + j, rtx + i), 0), 4)
+        
+        if tx == 8
+            if tileChoice == 1 || tileChoice == 3
+                tx += 8
+            end
+        end
+        if ty == 8
+            if tileChoice == 2 || tileChoice == 3
+                ty += 8
+            end
+        end
+        if !isEdge
+            tx -= 8
+            ty -= 8
+        end
+        Ahorn.drawImage(ctx, isEdge ? block : inside, (i - 1) * 8, (j - 1) * 8, tx, ty, 8, 8)
+    end
+    Ahorn.drawImage(ctx, eyeSprite, div(width - eyeSprite.width, 2), div(height - eyeSprite.height, 2))
+end
+
+end
---
+++ src/Entities/Misc/Melvin.cs
@@ -0,0 +1,140 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using static Celeste.CrushBlock;
+
+namespace Celeste.Mod.CommunalHelper.Entities {
+
+    [CustomEntity("CommunalHelper/Melvin")]
+    class Melvin : Solid {
+
+        private Axes axis;
+
+        private List<Image> activeTopTiles = new List<Image>();
+        private List<Image> activeBottomTiles = new List<Image>();
+        private List<Image> activeRightTiles = new List<Image>();
+        private List<Image> activeLeftTiles = new List<Image>();
+        private float topTilesAlpha, bottomTilesAlpha, leftTilesAlpha, rightTilesAlpha;
+
+        public Melvin(EntityData data, Vector2 offset)
+            : this(data.Position + offset, data.Width, data.Height, data.Enum("axes", Axes.Both)) { }
+
+        public Melvin(Vector2 position, int width, int height, Axes axis)
+            : base(position, width, height, safe: false) {
+
+            int w = (int) (base.Width / 8f);
+            int h = (int) (base.Height / 8f);
+
+            this.axis = axis;
+
+            string blockAxis = axis switch {
+                Axes.Vertical => "surf_V",
+                Axes.Horizontal => "surf_H",
+                _ => "surf_A"
+            };
+
+            MTexture litUp = GFX.Game["objects/CommunalHelper/melvin/lit_up_" + (axis == Axes.Both ? "full" : "cut")];
+            MTexture litDown = GFX.Game["objects/CommunalHelper/melvin/lit_down_" + (axis == Axes.Both ? "full" : "cut")];
+            MTexture litLeft = GFX.Game["objects/CommunalHelper/melvin/lit_left_" + (axis == Axes.Both ? "full" : "cut")];
+            MTexture litRight = GFX.Game["objects/CommunalHelper/melvin/lit_right_" + (axis == Axes.Both ? "full" : "cut")];
+
+            MTexture block = GFX.Game["objects/CommunalHelper/melvin/" + blockAxis];
+            MTexture inside = GFX.Game["objects/CommunalHelper/melvin/inside"];
+
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    bool horizontalEdge = i == 0 || i == w - 1; // left and right edges
+                    bool verticalEdge = j == 0 || j == h - 1; // bottom and top edges
+                    bool edge = horizontalEdge || verticalEdge; // both
+                    bool corner = horizontalEdge && verticalEdge; // corner
+
+                    int tx = (i == 0) ? 0 : ((i == w - 1) ? 3 : Calc.Random.Choose(1, 2));
+                    int ty = (j == 0) ? 0 : ((j == h - 1) ? 3 : Calc.Random.Choose(1, 2));
+                    if (!edge) { tx -= 1; ty -= 1; }
+
+                    Image image = new Image((edge ? block : inside).GetSubtexture(tx * 8, ty * 8, 8, 8));
+                    image.Position = new Vector2(i * 8, j * 8);
+                    Add(image);
+
+                    if (edge) {
+                        if ((corner && axis == Axes.Both) || (horizontalEdge && (axis == Axes.Horizontal || axis == Axes.Both))) {
+                            bool left = i == 0;
+                            Image litEdge = new Image((left ? litLeft : litRight).GetSubtexture(0, ty * 8, 8, 8)) {
+                                Position = image.Position,
+                                Color = Color.Transparent
+                            };
+                            (left ? activeLeftTiles : activeRightTiles).Add(litEdge);
+                            Add(litEdge);
+                        }
+                        if ((corner && axis == Axes.Both) || (verticalEdge && (axis == Axes.Vertical || axis == Axes.Both))) {
+                            bool top = j == 0;
+                            Image litEdge = new Image((top ? litUp : litDown).GetSubtexture(tx * 8, 0, 8, 8)) {
+                                Position = image.Position,
+                                Color = Color.Transparent
+                            };
+                            (top ? activeTopTiles : activeBottomTiles).Add(litEdge);
+                            Add(litEdge);
+                        }
+                    }
+                }
+            }
+            OnDashCollide = OnDashed;
+            Add(new LightOcclude(0.2f));
+            //Add(new BloomPoint(new Vector2(width / 2, height / 2), .5f, (width + height) / 2)); // experimental bloom
+        }
+
+        public DashCollisionResults OnDashed(Player player, Vector2 dir) {
+            ActivateTiles(dir.Y == 1, dir.Y == -1, dir.X == 1, dir.X == -1);
+            Audio.Play("event:/game/06_reflection/crushblock_activate", base.Center);
+            return DashCollisionResults.Bounce;
+        }
+
+        private void ActivateTiles(bool up, bool down, bool left, bool right) {
+            if (up)
+                topTilesAlpha = 1f;
+            if (down)
+                bottomTilesAlpha = 1f;
+            if (left)
+                leftTilesAlpha = 1f;
+            if (right)
+                rightTilesAlpha = 1f;
+        }
+             
+        private void UpdateActiveTiles() {
+            topTilesAlpha = Calc.Approach(topTilesAlpha, 0f, Engine.DeltaTime * 0.5f);
+            bottomTilesAlpha = Calc.Approach(bottomTilesAlpha, 0f, Engine.DeltaTime * 0.5f);
+            leftTilesAlpha = Calc.Approach(leftTilesAlpha, 0f, Engine.DeltaTime * 0.5f);
+            rightTilesAlpha = Calc.Approach(rightTilesAlpha, 0f, Engine.DeltaTime * 0.5f);
+
+            foreach (Image tile in activeTopTiles) {
+                tile.Color = Color.White * topTilesAlpha;
+            }
+            foreach (Image tile in activeBottomTiles) {
+                tile.Color = Color.White * bottomTilesAlpha;
+            }
+            foreach (Image tile in activeLeftTiles) {
+                tile.Color = Color.White * leftTilesAlpha;
+            }
+            foreach (Image tile in activeRightTiles) {
+                tile.Color = Color.White * rightTilesAlpha;
+            }
+        }
+
+        public override void Update() {
+            base.Update();
+            UpdateActiveTiles();
+        }
+
+        public override void Render() {
+            Vector2 position = Position;
+            Position += Shake;
+
+            base.Render();
+
+            Position = position;
+        }
+
+    }
+}
