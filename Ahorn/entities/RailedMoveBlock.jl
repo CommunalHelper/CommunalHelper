0c8bac401dfd87ca38fcffdff7c717222abe8295
catapillie <espritox@gmail.com>
2021-04-16 19:17:22 +0200

    Updated RailedMoveBlock rendering in Ahorn, added steering modes

--- Ahorn/entities/RailedMoveBlock.jl
+++ Ahorn/entities/RailedMoveBlock.jl
@@ -2,20 +2,25 @@ module CommunalHelperRailedMoveBlock
 
 using ..Ahorn, Maple
 
+const steeringModes = ["Horizontal", "Vertical", "Both"]
+
 @mapdef Entity "CommunalHelper/RailedMoveBlock" RailedMoveBlock(
 			x::Integer, y::Integer,
 			width::Integer=16, height::Integer=16,
-			nodes::Array{Tuple{Integer, Integer}, 1}=Tuple{Integer, Integer}[])
+			nodes::Array{Tuple{Integer, Integer}, 1}=Tuple{Integer, Integer}[],
+            steeringMode::String="Horizontal")
 
 const placements = Ahorn.PlacementDict(
-    "Railed Move Block (Communal Helper)" => Ahorn.EntityPlacement(
+    "Railed Move Block ($steeringMode) (Communal Helper)" => Ahorn.EntityPlacement(
         RailedMoveBlock,
 		"rectangle",
-		Dict{String, Any}(),
+		Dict{String, Any}(
+            "steeringMode" => steeringMode
+        ),
 		function(entity)
             entity.data["nodes"] = [(Int(entity.data["x"]) + Int(entity.data["width"]) + 8, Int(entity.data["y"]))]
         end
-    )
+    ) for steeringMode in steeringModes
 )
 
 Ahorn.nodeLimits(entity::RailedMoveBlock) = 1, 1
@@ -23,8 +28,15 @@ Ahorn.nodeLimits(entity::RailedMoveBlock) = 1, 1
 Ahorn.minimumSize(entity::RailedMoveBlock) = 16, 16
 Ahorn.resizable(entity::RailedMoveBlock) = true, true
 
+Ahorn.editingOptions(entity::RailedMoveBlock) = Dict{String, Any}(
+   "steeringMode" => steeringModes
+)
+
 const midColor = (4, 3, 23) ./ 255
 const highlightColor = (59, 50, 101) ./ 255
+const buttonColor = (71, 64, 112, 255) ./ 255
+
+const button = "objects/moveBlock/button"
 
 function Ahorn.selection(entity::RailedMoveBlock)
     x, y = Ahorn.position(entity)
@@ -76,7 +88,21 @@ function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::RailedMoveBlock,
     tilesWidth = div(width, 8)
     tilesHeight = div(height, 8)
 
-    frame = "objects/moveBlock/base"
+    steeringMode = get(entity.data, "steeringMode", "Horizontal")
+    both = true
+    horizontal = false
+    vertical = false
+
+    frame = "objects/CommunalHelper/railedMoveBlock/base_both"
+    if steeringMode == "Horizontal"
+        frame = "objects/moveBlock/base_v"
+        both = false
+        horizontal = true
+    elseif steeringMode == "Vertical"
+        frame = "objects/moveBlock/base_h"
+        both = false
+        vertical = true
+    end
 
     Ahorn.drawRectangle(ctx, x + 2, y + 2, width - 4, height - 4, highlightColor, highlightColor)
     Ahorn.drawRectangle(ctx, x + 8, y + 8, width - 16, height - 16, midColor)
@@ -84,17 +110,55 @@ function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::RailedMoveBlock,
     for i in 2:tilesWidth - 1
         Ahorn.drawImage(ctx, frame, x + (i - 1) * 8, y, 8, 0, 8, 8)
         Ahorn.drawImage(ctx, frame, x + (i - 1) * 8, y + height - 8, 8, 16, 8, 8)
+
+        if both || vertical
+            Ahorn.drawImage(ctx, button, x + (i - 1) * 8, y - 2, 6, 0, 8, 6, tint=buttonColor)
+        end
     end
 
     for i in 2:tilesHeight - 1
         Ahorn.drawImage(ctx, frame, x, y + (i - 1) * 8, 0, 8, 8, 8)
         Ahorn.drawImage(ctx, frame, x + width - 8, y + (i - 1) * 8, 16, 8, 8, 8)
+
+        if both || horizontal
+            Ahorn.Cairo.save(ctx)
+
+            Ahorn.rotate(ctx, -pi / 2)
+            Ahorn.drawImage(ctx, button, -y + i * 8 - height - 8, x - 2, 6, 0, 8, 6, tint=buttonColor)
+            Ahorn.scale(ctx, 1, -1)
+            Ahorn.drawImage(ctx, button, -y + i * 8 - height - 8, -x - 2 - width, 6, 0, 8, 6, tint=buttonColor)
+
+            Ahorn.Cairo.restore(ctx)
+        end
     end
 
     Ahorn.drawImage(ctx, frame, x, y, 0, 0, 8, 8)
     Ahorn.drawImage(ctx, frame, x + width - 8, y, 16, 0, 8, 8)
     Ahorn.drawImage(ctx, frame, x, y + height - 8, 0, 16, 8, 8)
     Ahorn.drawImage(ctx, frame, x + width - 8, y + height - 8, 16, 16, 8, 8)
+
+    if both || vertical
+        Ahorn.Cairo.save(ctx)
+
+        Ahorn.drawImage(ctx, button, x + 2, y - 2, 0, 0, 6, 6, tint=buttonColor)
+        Ahorn.scale(ctx, -1, 1)
+        Ahorn.drawImage(ctx, button, -x + 2 - width, y - 2, 0, 0, 6, 6, tint=buttonColor)
+
+        Ahorn.Cairo.restore(ctx)
+    end
+
+    if both || horizontal
+        Ahorn.Cairo.save(ctx)
+
+        Ahorn.rotate(ctx, -pi / 2)
+        Ahorn.drawImage(ctx, button, -y - height + 2, x - 2, 0, 0, 8, 6, tint=buttonColor)
+        Ahorn.drawImage(ctx, button, -y - 10, x - 2, 14, 0, 8, 6, tint=buttonColor)
+        Ahorn.scale(ctx, 1, -1)
+        Ahorn.drawImage(ctx, button, -y - height + 2, -x - 2 -width, 0, 0, 8, 6, tint=buttonColor)
+        Ahorn.drawImage(ctx, button, -y - 10, -x - 2 - width, 14, 0, 8, 6, tint=buttonColor)
+
+        Ahorn.Cairo.restore(ctx)
+    end
 
     # Ahorn.drawRectangle(ctx, div(width - arrowSprite.width, 2) + 1, div(height - arrowSprite.height, 2) + 1, 8, 8, highlightColor, highlightColor)
     # Ahorn.drawImage(ctx, arrowSprite, div(width - arrowSprite.width, 2), div(height - arrowSprite.height, 2))
