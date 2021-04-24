e6e8e09a2dbb49fecf8208da90cd5b5bd1746704
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-04-24 01:28:45 -0700

    Add SoundSurfacePanel

---
+++ Ahorn/entities/panels/SurfaceSoundPanel.jl
@@ -0,0 +1,64 @@
+module CommunalHelperSurfaceSoundPanel
+
+using ..Ahorn, Maple
+using Ahorn.CommunalHelper
+
+@mapdef Entity "CommunalHelper/SurfaceSoundPanel" Panel(x::Integer, y::Integer, 
+   soundIndex::Integer=11)
+
+const placements = Ahorn.PlacementDict(
+   "Sound Surface Panel ($dir) (Communal Helper)" => Ahorn.EntityPlacement(
+      Panel,
+      "rectangle",
+      Dict{String, Any}(
+         "orientation" => dir
+      ),
+   ) for dir in Maple.spike_directions
+)
+
+Ahorn.editingOptions(entity::Panel) = Dict{String, Any}(
+   "orientation" => Maple.spike_directions,
+   "soundIndex" => CommunalHelper.surfaceSounds
+)
+
+Ahorn.minimumSize(entity::Panel) = 8, 8
+
+const resizeDirections = Dict{String, Tuple{Bool, Bool}}(
+   "Up" => (true, false),
+   "Down" => (true, false),
+   "Left" => (false, true),
+   "Right" => (false, true),
+)
+
+function Ahorn.resizable(entity::Panel)
+   orientation = get(entity.data, "orientation", "Up")
+   return resizeDirections[orientation]
+end
+
+function Ahorn.selection(entity::Panel)
+   x, y = Ahorn.position(entity)
+
+   width = Int(get(entity.data, "width", 8))
+   height = Int(get(entity.data, "height", 8))
+
+   orientation = get(entity.data, "orientation", "Up")
+
+   return Ahorn.Rectangle(x, y, width, height)
+end
+
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::Panel)
+   orientation = get(entity.data, "orientation", "Up")
+
+   width = Int(get(entity.data, "width", 8))
+   height = Int(get(entity.data, "height", 8))
+   
+   x, y = Ahorn.position(entity)
+   ox, oy = x + width * (orientation == "Right"), y + height * (orientation == "Down")
+   cx, cy = x + width * in(orientation, ("Up", "Down", "Right")), y + height * in(orientation, ("Down", "Left", "Right"))
+
+   Ahorn.drawRectangle(ctx, x, y, width, height, (1.0, 0.5, 0.0, 0.4))
+   Ahorn.drawLines(ctx, ((ox, oy), (cx, cy)), (1.0, 1.0, 1.0, 1.0), thickness=1)
+
+end
+
+end 
---
+++ src/Entities/Panels/SurfaceSoundPanel.cs
@@ -0,0 +1,23 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.CommunalHelper.Entities {
+    [CustomEntity("CommunalHelper/SurfaceSoundPanel")]
+    public class SurfaceSoundPanel : AbstractPanel {
+
+        #region Loading
+
+        public static Entity Load(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
+            Spikes.Directions orientation = entityData.Enum<Spikes.Directions>("orientation");
+            return new SurfaceSoundPanel(entityData.Position + offset, GetSize(entityData, orientation), orientation, entityData.Int("soundIndex", SurfaceIndex.DreamBlockInactive));
+        }
+
+        #endregion
+
+        public SurfaceSoundPanel(Vector2 position, float size, Spikes.Directions orientation, int soundIndex) 
+            : base(position, size, orientation, false) {
+            surfaceSoundIndex = soundIndex;
+        }
+    }
+}
