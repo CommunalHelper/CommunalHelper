e70ac95f28d8ab5dd8a26c01dc576095066da865
catapillie <espritox@gmail.com>
2021-05-14 15:57:44 +0200

    Added solid tiles group entity

---
+++ src/Entities/Misc/SolidTilesGroup.cs
@@ -0,0 +1,20 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.CommunalHelper.Entities {
+    [CustomEntity("CommunalHelper/SolidTilesGroup")]
+    class SolidTilesGroup : ConnectedSolid {
+
+        public SolidTilesGroup(EntityData data, Vector2 offset) :
+            this(data.Position + offset, data.Width, data.Height) { }
+
+        public SolidTilesGroup(Vector2 position, int width, int height) :
+            base(position, width, height, safe: true) {
+            Collidable = false;
+        }
+
+
+    }
+}
