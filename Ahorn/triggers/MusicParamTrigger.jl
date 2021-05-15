4095bb5d08acf5ef6565bd931cfacdc1164ba32b
catapillie <espritox@gmail.com>
2021-05-15 19:56:58 +0200

    Changed tab indentation to 4 space characters indentation

--- Ahorn/triggers/MusicParamTrigger.jl
+++ Ahorn/triggers/MusicParamTrigger.jl
@@ -3,20 +3,20 @@ module CommunalHelperMusicParamTrigger
 using ..Ahorn, Maple
 
 @mapdef Trigger "CommunalHelper/MusicParamTrigger" MusicParamTrigger(
-	x::Integer, 
-	y::Integer, 
-	width::Integer = Maple.defaultTriggerWidth, 
-	height::Integer = Maple.defaultTriggerHeight,
-	param::String="", 
-	enterValue::Number=1.0, 
-	exitValue::Number=0.0,
+    x::Integer, 
+    y::Integer, 
+    width::Integer = Maple.defaultTriggerWidth, 
+    height::Integer = Maple.defaultTriggerHeight,
+    param::String="", 
+    enterValue::Number=1.0, 
+    exitValue::Number=0.0,
 )
 
 const placements = Ahorn.PlacementDict(
-	"Music Paramater Trigger (Communal Helper)" => Ahorn.EntityPlacement(
-		MusicParamTrigger,
-		"rectangle",
-	),
+    "Music Paramater Trigger (Communal Helper)" => Ahorn.EntityPlacement(
+        MusicParamTrigger,
+        "rectangle",
+    ),
 )
 
 end
