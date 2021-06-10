7ddab5fcc8501aeb62e6d98e459a2581f7d5860d
catapillie <espritox@gmail.com>
2021-06-10 20:23:01 +0200

    Added DreamJellyfish by extending Glider, added DreamDashCollider component

---
+++ src/Entities/DreamStuff/DreamDashCollider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.CommunalHelper.Entities {
+    [Tracked]
+    class DreamDashCollider : Component {
+        public static readonly Color ActiveColor = Color.Teal;
+        public static readonly Color InactiveColor = Calc.HexToColor("044f63"); // darker teal
+
+        public Collider Collider;
+
+        public DreamDashCollider(Collider collider)
+            : base(active: true, visible: false) {
+            Collider = collider;
+        }
+
+        /// <summary>
+        /// Checks if the player is colliding with this component's associated entity.
+        /// </summary>
+        /// <param name="player">The player instance.</param>
+        /// <returns></returns>
+        public bool Check(Player player) {
+            Collider collider = Entity.Collider;
+
+            Entity.Collider = Collider;
+            bool check = player.CollideCheck(Entity);
+            Entity.Collider = collider;
+
+            return check;
+        }
+
+        public override void DebugRender(Camera camera) {
+            if (Collider != null) {
+                Collider collider = Entity.Collider;
+                
+                Entity.Collider = Collider;
+                Collider.Render(camera, Active ? ActiveColor : InactiveColor);
+                Entity.Collider = collider;
+            }
+        }
+    }
+}
---
+++ src/Entities/DreamStuff/DreamJellyfish.cs
@@ -0,0 +1,20 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.CommunalHelper.Entities {
+    [CustomEntity("CommunalHelper/DreamJellyfish")]
+    class DreamJellyfish : Glider {
+
+        public DreamJellyfish(EntityData data, Vector2 offset) 
+            : this(data.Position + offset, data.Bool("bubble"), data.Bool("tutorial")) { }
+
+        public DreamJellyfish(Vector2 position, bool bubble, bool tutorial)
+            : base(position, bubble, tutorial) {
+            Add(new DreamDashCollider(new Hitbox(28, 12, -13, -18)) {
+                Active = false,
+            });
+        }
+    }
+}
