7ae8fe4286974e01d03e5749f150a62953202db0
Luna <izzeldeen03@gmail.com>
2021-06-23 23:51:37 +0100

    fix a bunch of corner cases with players

--- src/Entities/ChainStuff/HardChain.cs
+++ src/Entities/ChainStuff/HardChain.cs
@@ -26,34 +26,35 @@ namespace Celeste.Mod.CommunalHelper.Entities.ChainStuff {
             } else {
                 // try to stop both objects by adding targetted solids
                 if (StartSolid != null)
-                    Constrain(StartSolid);
+                    Constrain(StartSolid, true);
                 if (EndSolid != null)
-                    Constrain(EndSolid);
+                    Constrain(EndSolid, false);
             }
         }
 
         private void CleanUpSolids() {
-            foreach (var item in TargetedSolids)
+            foreach (Entity item in TargetedSolids)
                 item.RemoveSelf();
         }
 
         // TODO: non-Solid Entities?
 
-        private void Constrain(Solid solid) {
+        private void Constrain(Solid solid, bool start) {
+            Func<Vector2> pos = start ? attachedEndGetter : attachedStartGetter;
             // compare corners to decide where to add boxes
-            if (solid.Top < attachedStartGetter.Invoke().Y) {
+            if (solid.Top < pos.Invoke().Y) {
                 // add top solid
                 AddSolid(solid.Position - new Vector2(0, 20), solid.Width, 20, solid);
             }
-            if (solid.Bottom > attachedStartGetter.Invoke().Y) {
+            if (solid.Bottom > pos.Invoke().Y) {
                 // add bottom solid
                 AddSolid(solid.Position + new Vector2(0, solid.Height), solid.Width, 20, solid);
             }
-            if (solid.Left < attachedStartGetter.Invoke().X) {
+            if (solid.Left < pos.Invoke().X) {
                 // add left solid
                 AddSolid(solid.Position - new Vector2(20, 0), 20, solid.Height, solid);
             }
-            if (solid.Right > attachedStartGetter.Invoke().X) {
+            if (solid.Right > pos.Invoke().X) {
                 // add right solid
                 AddSolid(solid.Position + new Vector2(solid.Width, 0), 20, solid.Height, solid);
             }
@@ -92,16 +93,23 @@ namespace Celeste.Mod.CommunalHelper.Entities.ChainStuff {
         #endregion
         #region Hooks
 
+        private static Entity current = null;
+
         internal static void Load() {
             On.Monocle.Entity.Update += MakeTargetedSolidsCollidable;
+            On.Celeste.Actor.MoveHExact += RecheckTargetedSolidsH;
+            On.Celeste.Actor.MoveVExact += RecheckTargetedSolidsV;
         }
 
         internal static void Unload() {
             On.Monocle.Entity.Update -= MakeTargetedSolidsCollidable;
+            On.Celeste.Actor.MoveHExact -= RecheckTargetedSolidsH;
+            On.Celeste.Actor.MoveVExact -= RecheckTargetedSolidsV;
         }
 
         private static void MakeTargetedSolidsCollidable(On.Monocle.Entity.orig_Update orig, Entity self) {
             List<TargetedSolid> ts = Solids.ContainsKey(self) ? Solids[self] : null;
+            current = self;
             if (ts != null)
                 foreach (TargetedSolid item in ts)
                     item.Collidable = true;
@@ -111,6 +119,43 @@ namespace Celeste.Mod.CommunalHelper.Entities.ChainStuff {
                     item.Collidable = false;
         }
 
+        private static bool RecheckTargetedSolidsV(On.Celeste.Actor.orig_MoveVExact orig, Actor self, int moveV, Collision onCollide, Solid pusher) {
+            List<TargetedSolid> ats = Solids.ContainsKey(self) ? Solids[self] : null;
+            List<TargetedSolid> cts = Solids.ContainsKey(current) ? Solids[current] : null;
+            if (cts != null)
+                foreach (TargetedSolid item in cts)
+                    item.Collidable = false;
+            if (ats != null)
+                foreach (TargetedSolid item in ats)
+                    item.Collidable = true;
+            bool ret = orig(self, moveV, onCollide, pusher);
+            if (ats != null)
+                foreach (TargetedSolid item in ats)
+                    item.Collidable = false;
+            if (cts != null)
+                foreach (TargetedSolid item in cts)
+                    item.Collidable = true;
+            return ret;
+        }
+
+        private static bool RecheckTargetedSolidsH(On.Celeste.Actor.orig_MoveHExact orig, Actor self, int moveH, Collision onCollide, Solid pusher) {
+            List<TargetedSolid> ats = Solids.ContainsKey(self) ? Solids[self] : null;
+            List<TargetedSolid> cts = Solids.ContainsKey(current) ? Solids[current] : null;
+            if (cts != null)
+                foreach (TargetedSolid item in cts)
+                    item.Collidable = false;
+            if (ats != null)
+                foreach (TargetedSolid item in ats)
+                    item.Collidable = true;
+            bool ret = orig(self, moveH, onCollide, pusher);
+            if (ats != null)
+                foreach (TargetedSolid item in ats)
+                    item.Collidable = false;
+            if (cts != null)
+                foreach (TargetedSolid item in cts)
+                    item.Collidable = true;
+            return ret;
+        }
         #endregion
     }
 }
