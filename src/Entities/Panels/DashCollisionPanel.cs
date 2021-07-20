aff6f02f9045f822b57f1d4b012f2bea8d3cabea
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-07-20 14:01:42 -0700

    Add DashCollision Panel

---
+++ src/Entities/Panels/DashCollisionPanel.cs
@@ -0,0 +1,33 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.CommunalHelper.Entities {
+    [CustomEntity("CommunalHelper/DashCollisionPanel")]
+    public class DashCollisionPanel : AbstractPanel {
+
+        #region Loading
+
+        public static Entity Load(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
+            Spikes.Directions orientation = entityData.Enum<Spikes.Directions>("orientation");
+            return new DashCollisionPanel(entityData, offset, orientation);
+        }
+
+        #endregion
+
+        public DashCollisionResults dashCollisionOverride;
+        public bool overrideCollision;
+
+        public DashCollisionPanel(EntityData data, Vector2 offset, Spikes.Directions orientation)
+            : base(data.Position + offset, GetSize(data, orientation), orientation, false) {
+            overrideCollision = data.Bool("overrideCollision");
+            dashCollisionOverride = data.Enum("dashCollideResult", DashCollisionResults.NormalCollision);
+        }
+
+        protected override DashCollisionResults OnDashCollide(Player player, Vector2 dir) {
+            if (!overrideCollision)
+                base.OnDashCollide(player, dir);
+            return dashCollisionOverride;
+        }
+    }
+}
