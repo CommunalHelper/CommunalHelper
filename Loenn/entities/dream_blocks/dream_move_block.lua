7861a1e7bb489a9afcc61eb98e718d7e071750cc
catapillie <espritox@gmail.com>
2022-03-27 00:54:21 +0100

    Add Dream Move Block plugin

---
+++ Loenn/entities/dream_blocks/dream_move_block.lua
@@ -0,0 +1,75 @@
+local drawableNinePatch = require("structs.drawable_nine_patch")
+local drawableSprite = require("structs.drawable_sprite")
+local utils = require("utils")
+local communalHelper = require("mods").requireFromPlugin("libraries.communal_helper")
+
+local dreamMoveBlock = {}
+
+local directions = {"Up", "Down", "Left", "Right"}
+
+dreamMoveBlock.name = "CommunalHelper/DreamMoveBlock"
+dreamMoveBlock.minimumSize = {16, 16}
+dreamMoveBlock.fieldInformation = {
+    direction = {
+        options = directions,
+        editable = false,
+    }
+}
+
+function dreamMoveBlock.depth(room, entity)
+    return entity.below and 5000 or -11000
+end
+
+dreamMoveBlock.placements = {
+    {
+        name = "dream_move_block",
+        data = {
+            width = 16,
+            height = 16,
+            featherMode = false,
+            oneUse = false,
+            refillCount = -1,
+            below = false,
+            quickDestroy = false,
+            direction = "Right",
+            moveSpeed = 60.0,
+            noCollide = false,
+        }
+    }
+}
+
+local arrowTextures = {
+    up = "objects/CommunalHelper/dreamMoveBlock/arrow02",
+    left = "objects/CommunalHelper/dreamMoveBlock/arrow04",
+    right = "objects/CommunalHelper/dreamMoveBlock/arrow00",
+    down = "objects/CommunalHelper/dreamMoveBlock/arrow06"
+}
+
+function dreamMoveBlock.sprite(room, entity)
+    local x, y = entity.x or 0, entity.y or 0
+    local width, height = entity.width or 16, entity.height or 16
+    local feather = entity.featherMode
+
+    local sprites = {}
+
+    table.insert(sprites, communalHelper.getCustomDreamBlockSprites(x, y, width, height, feather))
+    
+    local direction = string.lower(entity.direction)
+    local arrowTexture = arrowTextures[direction] or arrowTextures["right"]
+
+    local arrowSprite = drawableSprite.fromTexture(arrowTexture, entity)
+    arrowSprite:addPosition(math.floor(width / 2), math.floor(height / 2))
+    arrowSprite.depth = -11
+    table.insert(sprites, arrowSprite)
+
+    return sprites
+end
+
+function dreamMoveBlock.selection(room, entity)
+    local x, y = entity.x or 0, entity.y or 0
+    local width, height = entity.width or 16, entity.height or 16
+
+    return utils.rectangle(x, y, width, height)
+end
+
+return dreamMoveBlock
