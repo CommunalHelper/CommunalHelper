31bf0012520b43046d48f4c6312147f894bb01a8
catapillie <espritox@gmail.com>
2022-03-27 12:41:55 +0200

    Add custom Summit Gem dropdown options

--- Loenn/entities/summit_gem.lua
+++ Loenn/entities/summit_gem.lua
@@ -1,7 +1,23 @@
 local summitGem = {}
 
+local summitGemNames = {
+    ["0 - Reflection"] = 0,
+    ["1 - Forsaken City"] = 1,
+    ["2 - Old Site"] = 2,
+    ["3 - Celestial Resort"] = 3,
+    ["4 - Golden Ridge"] = 4,
+    ["5 - Mirror Temple"] = 5,
+    ["6 - Custom Blue Star Gem"] = 6,
+    ["7 - Custom Red Triangle Gem"] = 7,
+}
+
 summitGem.name = "CommunalHelper/CustomSummitGem"
 summitGem.depth = 0
+summitGem.fieldInformation = {
+    index = {
+        options = summitGemNames
+    }
+}
 
 summitGem.placements = {
     {
