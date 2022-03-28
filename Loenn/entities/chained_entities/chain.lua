2f93b41230d8d1473856d14b5b2533a54e4f528c
catapillie <espritox@gmail.com>
2022-03-28 20:29:07 +0200

    Add Chain entity plugin

---
+++ Loenn/entities/chained_entities/chain.lua
@@ -0,0 +1,46 @@
+local drawing = require("utils.drawing")
+local utils = require("utils")
+local drawableLine = require("structs.drawable_line")
+
+local chain = {}
+
+chain.name = "CommunalHelper/Chain"
+chain.nodeLimits = {1, 1}
+
+chain.placements = {
+    name = "chain",
+    data = {
+        extraJoints = 0,
+        outline = true,
+    }
+}
+
+function chain.sprite(room, entity)
+    local firstNode = entity.nodes[1]
+
+    local start = {entity.x, entity.y}
+    local stop = {firstNode.x, firstNode.y}
+    local control = {
+        (start[1] + stop[1]) / 2,
+        (start[2] + stop[2]) / 2 + 24
+    }
+
+    local points = drawing.getSimpleCurve(start, stop, control)
+
+    return drawableLine.fromPoints(points, {0.0, 0.5, 0.5, 1.0}, 1)
+end
+
+function chain.selection(room, entity)
+    local main = utils.rectangle(entity.x - 2, entity.y - 2, 5, 5)
+    local nodes = {}
+
+    if entity.nodes then
+        for i, node in ipairs(entity.nodes) do
+            nodes[i] = utils.rectangle(node.x - 2, node.y - 2, 5, 5)
+        end
+    end
+
+    return main, nodes
+end
+
+return chain
