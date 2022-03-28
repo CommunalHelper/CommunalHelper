ccf9b06b499aae82aae54a7ce8f76758d4634f50
catapillie <espritox@gmail.com>
2022-03-28 21:49:57 +0200

    Add a few simple entity plugins

---
+++ Loenn/entities/heart_gem_shards.lua
@@ -0,0 +1,29 @@
+local drawableSprite = require("structs.drawable_sprite")
+
+local heartGemShards = {}
+
+heartGemShards.name = "CommunalHelper/CrystalHeart"
+heartGemShards.depth = -2000000
+heartGemShards.nodeLimits = {1, -1}
+heartGemShards.nodeVisibility = "always"
+heartGemShards.nodeLineRenderType = "fan"
+
+heartGemShards.placements = {
+    {
+        name = "heart_gem_shards",
+        data = {
+            removeCameraTriggers = false,
+        }
+    }
+}
+
+heartGemShards.texture = "collectables/heartGem/ghost00"
+
+function heartGemShards.nodeSprite(room, entity, node, nodeIndex, viewport)
+    return {
+        drawableSprite.fromTexture("collectables/CommunalHelper/heartGemShard/shard_outline0" .. (nodeIndex % 3), node),
+        drawableSprite.fromTexture("collectables/CommunalHelper/heartGemShard/shard0" .. (nodeIndex % 3), node)
+    }
+end
+
+return heartGemShards
---
+++ Loenn/entities/input_flag_controller.lua
@@ -0,0 +1,20 @@
+local inputFlagController = {}
+
+inputFlagController.name = "CommunalHelper/InputFlagController"
+inputFlagController.depth = -1000000
+inputFlagController.texture = "objects/CommunalHelper/inputFlagController/icon"
+
+inputFlagController.placements = {
+    {
+        name = "controller",
+        data = {
+            flags = "",
+            toggle = true,
+            resetFlags = false,
+            delay = 0.0,
+            grabOverride = false,
+        }
+    }
+}
+
+return inputFlagController
