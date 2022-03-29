a442f9b5bd2d3ea3f13915b095e36c3daba69f6d
catapillie <espritox@gmail.com>
2022-03-29 23:52:09 +0200

    Add all 4 trigger plugins

---
+++ Loenn/triggers/music_param.lua
@@ -0,0 +1,12 @@
+return {
+    name = "CommunalHelper/MusicParamTrigger",
+
+    placements = {
+        name = "trigger",
+        data = {
+            param = "",
+            enterValue = 1.0,
+            exitValue = 0.0,
+        }
+    }
+}
---
+++ Loenn/triggers/sound_area.lua
@@ -0,0 +1,12 @@
+return {
+    name = "CommunalHelper/SoundAreaTrigger",
+
+    nodeLimits = {1, 1},
+
+    placements = {
+        name = "trigger",
+        data = {
+            event = "",
+        }
+    }
+}
