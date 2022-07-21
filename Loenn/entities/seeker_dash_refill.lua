62ffd63b4c21eac9ed5f78cf64a5f3d13edaf47f
catapillie <espritox@gmail.com>
2022-07-21 05:15:33 +0200

    Adjust Seeker Dash Refill selection

--- Loenn/entities/seeker_dash_refill.lua
+++ Loenn/entities/seeker_dash_refill.lua
@@ -1,3 +1,5 @@
+local utils = require("utils")
+
 local seekerDashRefill = {}
 
 seekerDashRefill.name = "CommunalHelper/SeekerDashRefill"
@@ -13,5 +15,10 @@ seekerDashRefill.placements = {
 }
 
 seekerDashRefill.texture = "objects/CommunalHelper/seekerDashRefill/idle00"
+
+function seekerDashRefill.selection(room, entity)
+    local x, y = entity.x, entity.y
+    return utils.rectangle(x - 5, y - 5, 10, 10)
+end
 
 return seekerDashRefill
