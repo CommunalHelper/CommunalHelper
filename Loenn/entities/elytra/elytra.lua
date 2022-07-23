be2cefdbca4f4aa81e88b7933cf16bb833e5d6ed
catapillie <espritox@gmail.com>
2022-07-23 16:45:21 +0200

    Add placeholder Elytra plugin

---
+++ Loenn/entities/elytra/elytra.lua
@@ -0,0 +1,12 @@
+local elytra = {}
+
+elytra.name = "CommunalHelper/Elytra"
+elytra.depth = 0
+
+elytra.placements = {
+    name = "elytra"
+}
+
+elytra.texture = "objects/flyFeather/idle00"
+
+return elytra
