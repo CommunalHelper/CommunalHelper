88443c37fd683f8048acf9b876f394299dca7c6f
catapillie <espritox@gmail.com>
2022-07-24 04:37:42 +0200

    Add custom animation metadata, and correct hair position depending on player sprite rotation

--- src/Entities/Elytra/ElytraState.cs
+++ src/Entities/Elytra/ElytraState.cs
@@ -1,12 +1,21 @@
 ﻿using FMOD.Studio;
 using Microsoft.Xna.Framework;
+using Mono.Cecil.Cil;
 using Monocle;
+using MonoMod.Cil;
 using MonoMod.Utils;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Celeste.Mod.CommunalHelper.Entities {
     public static class ElytraState {
+        // PlayerSprite.FrameMetadata is never set somewhere else, except when initialized, so keeping the instance here should be okay.
+        private static readonly Dictionary<string, PlayerAnimMetadata> PlayerSprite_FrameMetadata
+            = (Dictionary<string, PlayerAnimMetadata>) typeof(PlayerSprite).GetField("FrameMetadata", BindingFlags.Static | BindingFlags.NonPublic)
+                                                                           .GetValue(null);
+
         public static int St { get; private set; } = -1;
 
         private const string CommunalHelper_Player_glideAngle           = "CommunalHelper_Player_glideAngle"; // float
@@ -31,7 +40,10 @@ namespace Celeste.Mod.CommunalHelper.Entities {
 
             On.Celeste.Player.Die += Mod_Player_Die;
             On.Celeste.PlayerSprite.ctor += Mod_PlayerSprite_ctor;
+            On.Celeste.PlayerSprite.CreateFramesMetadata += PlayerSprite_CreateFramesMetadata;
             On.Celeste.Player.UpdateSprite += Mod_Player_UpdateSprite;
+
+            IL.Celeste.PlayerHair.AfterUpdate += PlayerHair_AfterUpdate;
         }
 
         internal static void Unload() {
@@ -39,7 +51,39 @@ namespace Celeste.Mod.CommunalHelper.Entities {
 
             On.Celeste.Player.Die -= Mod_Player_Die;
             On.Celeste.PlayerSprite.ctor -= Mod_PlayerSprite_ctor;
+            On.Celeste.PlayerSprite.CreateFramesMetadata -= PlayerSprite_CreateFramesMetadata;
             On.Celeste.Player.UpdateSprite -= Mod_Player_UpdateSprite;
+
+            IL.Celeste.PlayerHair.AfterUpdate -= PlayerHair_AfterUpdate;
+        }
+
+        private static void PlayerHair_AfterUpdate(ILContext il) {
+            ILCursor cursor = new(il);
+
+            cursor.GotoNext(instr => instr.MatchLdloc(0));
+            cursor.GotoNext(MoveType.Before, instr => instr.OpCode == OpCodes.Callvirt);
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.EmitDelegate<Func<Vector2, PlayerHair, Vector2>>((pos, hair) => {
+                Player player = hair.Entity as Player;
+                if (player.StateMachine.State != St)
+                    return pos;
+
+                PlayerSprite sprite = player.Sprite;
+                Vector2 origin = sprite.RenderPosition;
+                return (pos - origin).Rotate(sprite.Rotation) + origin;
+            });
+        }
+
+        private static void PlayerSprite_CreateFramesMetadata(On.Celeste.PlayerSprite.orig_CreateFramesMetadata orig, string sprite) {
+            orig(sprite);
+
+            // hair="4,0|4,-1|3,-1|2,-1|2,-1|2,-1"
+            PlayerSprite_FrameMetadata[$"characters/{sprite}/CommunalHelper/fly00"] = new() { HasHair = true, HairOffset = new(4,  0), };
+            PlayerSprite_FrameMetadata[$"characters/{sprite}/CommunalHelper/fly01"] = new() { HasHair = true, HairOffset = new(4, -1), };
+            PlayerSprite_FrameMetadata[$"characters/{sprite}/CommunalHelper/fly02"] = new() { HasHair = true, HairOffset = new(3, -1), };
+            PlayerSprite_FrameMetadata[$"characters/{sprite}/CommunalHelper/fly03"] = new() { HasHair = true, HairOffset = new(2, -1), };
+            PlayerSprite_FrameMetadata[$"characters/{sprite}/CommunalHelper/fly04"] = new() { HasHair = true, HairOffset = new(2, -1), };
+            PlayerSprite_FrameMetadata[$"characters/{sprite}/CommunalHelper/fly05"] = new() { HasHair = true, HairOffset = new(2, -1), };
         }
 
         private static void Mod_Player_ctor(On.Celeste.Player.orig_ctor orig, Player self, Vector2 position, PlayerSpriteMode spriteMode) {
@@ -71,6 +115,7 @@ namespace Celeste.Mod.CommunalHelper.Entities {
             if (self.StateMachine.State == St) {
                 self.Sprite.Play(GlidingAnimation);
 
+                // Determining which frame of the custom glide animation should be shown
                 DynData<Player> data = self.GetData();
                 int frame = 3;
                 if (data.Data.TryGetValue(CommunalHelper_Player_glideAngle, out object value)) {
