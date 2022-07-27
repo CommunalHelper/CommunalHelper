da2f68875678c2f0ca4a5657341fa1d64463e1b1
catapillie <espritox@gmail.com>
2022-07-27 05:38:18 +0200

    Display winged sprite for winged redless berries

--- Loenn/entities/berries/redless_berry.lua
+++ Loenn/entities/berries/redless_berry.lua
@@ -18,18 +18,25 @@ redlessBerry.placements = {
 
 local fruitTexture = "collectables/CommunalHelper/recolorableBerry/fruit00"
 local leavesTexture = "collectables/CommunalHelper/recolorableBerry/leaves00"
+local fruitWingedTexture = "collectables/CommunalHelper/recolorableBerry/fruit_wings00"
+local leavesWingedTexture = "collectables/CommunalHelper/recolorableBerry/leaves_wings00"
 
 -- will be changed in the future
 local color = {253 / 255, 191 / 255, 71 / 255, 255 / 255}
 
 function redlessBerry.sprite(room, entity)
+    local winged = entity.winged
+    local offset = winged and 1 or 0
+
     local sprites = {}
 
-    local fruit = drawableSprite.fromTexture(fruitTexture, entity)
+    local fruit = drawableSprite.fromTexture(winged and fruitWingedTexture or fruitTexture, entity)
     fruit.color = color
+    fruit:addPosition(0, offset)
     table.insert(sprites, fruit)
 
-    local leaves = drawableSprite.fromTexture(leavesTexture, entity)
+    local leaves = drawableSprite.fromTexture(winged and leavesWingedTexture or leavesTexture, entity)
+    leaves:addPosition(0, offset)
     table.insert(sprites, leaves)
 
     return sprites
