a53b528321b49cfea310b955d5f7e7ddb4e2380a
catapillie <espritox@gmail.com>
2022-10-22 20:09:13 +0200

    Add spiky option to Player Seeker Barrier plugin

--- Loenn/entities/player_seeker_barrier.lua
+++ Loenn/entities/player_seeker_barrier.lua
@@ -4,13 +4,29 @@ playerSeekerBarrier.name = "CommunalHelper/PlayerSeekerBarrier"
 playerSeekerBarrier.depth = 0
 
 playerSeekerBarrier.placements = {
-    name = "player_seeker_barrier",
-    data = {
-        width = 8,
-        height = 8
+    {
+        name = "normal",
+        data = {
+            width = 8,
+            height = 8,
+            spiky = false,
+        }
+    },
+    {
+        name = "spiky",
+        data = {
+            width = 8,
+            height = 8,
+            spiky = true,
+        }
     }
 }
 
-playerSeekerBarrier.color = {0.36, 0.36, 0.36, 0.8}
+local normalColor = {0.36, 0.36, 0.36, 0.8}
+local spikyColor = {0.36, 0.36, 0.36, 0.8}
+
+function playerSeekerBarrier.color(room, entity)
+    return entity.spiky and spikyColor or normalColor
+end
 
 return playerSeekerBarrier
