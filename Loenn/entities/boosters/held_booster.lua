284ccb043aff3ba01dc1a74dc8f955e14293307a
catapillie <espritox@gmail.com>
2022-12-25 09:41:59 +0100

    Make Held Booster plugin not rely on 'green' parameter

--- Loenn/entities/boosters/held_booster.lua
+++ Loenn/entities/boosters/held_booster.lua
@@ -6,47 +6,45 @@ local heldBooster = {}
 
 heldBooster.name = "CommunalHelper/HeldBooster"
 heldBooster.depth = -8500
+heldBooster.nodeLimits = {0, 1}
 heldBooster.nodeVisibility = "always"
-
-function heldBooster.ignoredFields(entity)
-    if entity.green then
-        return {"_name", "_id", "green"}
-    else
-        return {"_name", "_id", "green", "hidePath"}
-    end
-end
 
 heldBooster.placements = {
     {
         name = "purple",
         data = {
-            green = false
+            hidePath = false
         }
     },
     {
         name = "green",
         placementType = "line",
         data = {
-            green = true,
-            hidePath = false
+            hidePath = false,
+            nodes = {{x = 0, y = 0}} -- this is hacky
         }
     }
 }
+
+local function isGreen(entity)
+    local nodes = entity.nodes or {}
+    return #nodes == 1
+end
+
+function heldBooster.ignoredFields(entity)
+    if isGreen(entity) then
+        return {"_name", "_id", "green"}
+    else
+        return {"_name", "_id", "green", "hidePath", "nodes"}
+    end
+end
 
 local purpleTexture = "objects/CommunalHelper/boosters/heldBooster/purple/booster00"
 local greenTexture = "objects/CommunalHelper/boosters/heldBooster/green/booster00"
 local hiddenPathColor = {1.0, 1.0, 1.0, 0.25}
 
-function heldBooster.nodeLimits(room, entity)
-    if entity.green then
-        return 1, 1
-    else
-        return 0, 0
-    end
-end
-
 function heldBooster.sprite(room, entity)
-    if not entity.green then
+    if not isGreen(entity) then
         return drawableSprite.fromTexture(purpleTexture, entity)
     end
 
@@ -66,7 +64,7 @@ heldBooster.nodeColor = {1, 1, 1, 0.25}
 function heldBooster.selection(room, entity)
     local rect = utils.rectangle(entity.x - 9, entity.y - 9, 18, 18)
 
-    if not entity.green then
+    if not isGreen(entity) then
         return rect
     end
 
