2c4175be9ab719bd31e2893581d2dc44dca2a5c0
l-Luna <izzeldeen03@gmail.com>
2023-03-31 19:01:39 +0100

    bullet time controller entity

---
+++ src/Entities/Misc/BulletTimeController.cs
@@ -0,0 +1,33 @@
+﻿namespace Celeste.Mod.CommunalHelper.Entities.Misc;
+
+[CustomEntity("CommunalHelper/BulletTimeController")]
+public class BulletTimeController : Entity
+{
+    float timerate;
+    string flag;
+    bool active;
+
+    public BulletTimeController(EntityData data, Vector2 offset) : base(data.Position + offset)
+    {
+        Tag = Tags.PauseUpdate;
+        timerate = data.Float("speed");
+        flag = data.Attr("flag");
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        Player player = Scene.Tracker.GetEntity<Player>();
+        if (player is { IsIntroState: false, JustRespawned: false, Dashes: > 0 } && (string.IsNullOrEmpty(flag) || SceneAs<Level>().Session.GetFlag(flag)))
+        {
+            Engine.TimeRate = Scene.Paused ? 1 : timerate;
+            active = true;
+        }
+        else if (active)
+        {
+            Engine.TimeRate = 1;
+            active = false;
+        }
+    }
+}
