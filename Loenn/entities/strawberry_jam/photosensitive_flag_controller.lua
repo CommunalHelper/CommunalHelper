f8c82a11c9c83b03d24b12e6bdce8d6d70925403
l-Luna <izzeldeen03@gmail.com>
2023-04-02 17:08:24 +0100

    Photosensitive Flag Controller // move BTC icon to proper place

---
+++ Loenn/entities/strawberry_jam/photosensitive_flag_controller.lua
@@ -0,0 +1,15 @@
+local photosensitiveFlagController = {}
+
+photosensitiveFlagController.name = "CommunalHelper/SJ/PhotosensitiveFlagController"
+photosensitiveFlagController.depth = -1000000
+photosensitiveFlagController.texture = "objects/CommunalHelper/strawberryJam/photosensitiveFlagController/icon"
+photosensitiveFlagController.placements = {
+    {
+        name = "controller",
+        data = {
+            flag = ""
+        }
+    }
+}
+
+return photosensitiveFlagController
---
+++ src/Entities/StrawberryJam/PhotosensitiveFlagController.cs
@@ -0,0 +1,36 @@
+﻿namespace Celeste.Mod.CommunalHelper.Entities.StrawberryJam;
+
+[CustomEntity("CommunalHelper/SJ/PhotosensitiveFlagController")]
+public class PhotosensitiveFlagController : Entity
+{
+    public readonly string FlagName;
+
+    public PhotosensitiveFlagController(EntityData data, Vector2 offset) : base(data.Position + offset)
+    {
+        FlagName = data.Attr("flag");
+    }
+
+    public void UpdateFlag()
+    {
+        var session = SceneAs<Level>().Session;
+
+        if (FlagName.Length > 0 && session.GetFlag(FlagName) != Settings.Instance.DisableFlashes)
+        {
+            session.SetFlag(FlagName, Settings.Instance.DisableFlashes);
+        }
+    }
+
+    public override void Awake(Scene scene)
+    {
+        base.Awake(scene);
+
+        UpdateFlag();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        UpdateFlag();
+    }
+}
