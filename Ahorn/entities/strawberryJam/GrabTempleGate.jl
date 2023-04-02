138b837df97281bb57188485c52f97a006dad099
catapillie <espritox@gmail.com>
2023-04-02 17:38:44 +0200

    copy grab temple gate, loop block, solar elevator ahorn plugins

---
+++ Ahorn/entities/strawberryJam/GrabTempleGate.jl
@@ -0,0 +1,35 @@
+module CommunalHelperSJGrabTempleGate
+
+using ..Ahorn, Maple
+
+@mapdef Entity "CommunalHelper/SJ/GrabTempleGate" GrabTempleGate(x::Integer, y::Integer, closed::Bool = false)
+
+const placements = Ahorn.PlacementDict(
+    "Grab Temple Gate (Open) (Strawberry Jam) (Communal Helper)" => Ahorn.EntityPlacement(
+        GrabTempleGate,
+        "point",
+        Dict{String, Any}(
+            "closed" => false
+        )
+    ),
+    "Grab Temple Gate (Closed) (Strawberry Jam) (Communal Helper)" => Ahorn.EntityPlacement(
+        GrabTempleGate,
+        "point",
+        Dict{String, Any}(
+            "closed" => true
+        )
+    )
+)
+
+const texture = "objects/CommunalHelper/strawberryJam/grabTempleGate/TempleDoor05";
+
+function Ahorn.selection(entity::GrabTempleGate)
+    x, y = Ahorn.position(entity)
+    return Ahorn.Rectangle(x - 4, y, 15, 48)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::GrabTempleGate, room::Maple.Room)
+    Ahorn.drawImage(ctx, texture, -4, 0)
+end
+
+end
