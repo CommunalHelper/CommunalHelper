323be653b83fb66306ce874fe2d8b6091383afd9
l-Luna <izzeldeen03@gmail.com>
2023-04-12 18:05:26 +0100

    basic UFO impl

---
+++ Loenn/entities/misc/ufo.lua
@@ -0,0 +1,15 @@
+﻿local ufo = {}
+
+ufo.name = "CommunalHelper/UFO"
+ufo.depth = -8500
+ufo.texture = "objects/CommunalHelper/ufo/UFO"
+ufo.nodeLimits = {0, -1}
+ufo.placements = {
+    name = "ufo",
+    data = {
+        raySizeX = 13,
+        raySizeY = 60
+    }
+}
+
+return ufo
