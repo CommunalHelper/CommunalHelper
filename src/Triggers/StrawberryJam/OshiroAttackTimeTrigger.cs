3d16aed1c0de3ad8693367a66265d652ebd6ca1c
Vivian <71410564+Viv-0@users.noreply.github.com>
2023-10-22 15:55:12 -0400

    Update OshiroAttackTimeTrigger.cs
    Fix namespace

--- src/Triggers/StrawberryJam/OshiroAttackTimeTrigger.cs
+++ src/Triggers/StrawberryJam/OshiroAttackTimeTrigger.cs
@@ -6,35 +6,35 @@ using MonoMod.RuntimeDetour;
 using MonoMod.Utils;
 using System.Reflection;
 
-namespace Celeste.Mod.CommunalHelper.Triggers.StrawberryJam {
-	[CustomEntity("CommunalHelper/SJ/OshiroAttackTimeTrigger")]
-	public class OshiroAttackTimeTrigger : Trigger {
-		private bool Enable = true;
-		private static ILHook oshiroHook;
-		private static BindingFlags privateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
-		private static MethodInfo oshiroCoroutineInfo = typeof(AngryOshiro).GetMethod("ChaseCoroutine", privateInstance).GetStateMachineTarget();
-		public OshiroAttackTimeTrigger(EntityData data, Vector2 offset) : base(data, offset) {
-			Enable = data.Bool("Enable", true);
-		}
-		public static void Load() {
-			oshiroHook = new ILHook(oshiroCoroutineInfo, ModAttackTime);
-		}
-		public static void Unload() {
-			oshiroHook?.Dispose();
-			oshiroHook = null;
-		}
+namespace Celeste.Mod.CommunalHelper.Triggers.StrawberryJam;
 
-		private static void ModAttackTime(ILContext il) {
-			ILCursor cursor = new ILCursor(il);
-			while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<AreaKey>("Mode"))) {
-				cursor.EmitDelegate<Func<int, int>>(orig => CommunalHelperModule.Session.OshiroBsideTimer ? 1 : orig);
-				break;
-			}
-		}
+[CustomEntity("CommunalHelper/SJ/OshiroAttackTimeTrigger")]
+public class OshiroAttackTimeTrigger : Trigger {
+    private bool Enable = true;
+    private static ILHook oshiroHook;
+    private static BindingFlags privateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+    private static MethodInfo oshiroCoroutineInfo = typeof(AngryOshiro).GetMethod("ChaseCoroutine", privateInstance).GetStateMachineTarget();
+    public OshiroAttackTimeTrigger(EntityData data, Vector2 offset) : base(data, offset) {
+        Enable = data.Bool("Enable", true);
+    }
+    public static void Load() {
+        oshiroHook = new ILHook(oshiroCoroutineInfo, ModAttackTime);
+    }
+    public static void Unload() {
+        oshiroHook?.Dispose();
+        oshiroHook = null;
+    }
 
-		public override void OnEnter(Player player) {
-			base.OnEnter(player);
-            CommunalHelperModule.Session.OshiroBsideTimer = Enable;
-		}
-	}
+    private static void ModAttackTime(ILContext il) {
+        ILCursor cursor = new ILCursor(il);
+        while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<AreaKey>("Mode"))) {
+            cursor.EmitDelegate<Func<int, int>>(orig => CommunalHelperModule.Session.OshiroBsideTimer ? 1 : orig);
+            break;
+        }
+    }
+
+    public override void OnEnter(Player player) {
+        base.OnEnter(player);
+        CommunalHelperModule.Session.OshiroBsideTimer = Enable;
+    }
 }
