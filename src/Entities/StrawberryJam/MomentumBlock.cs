96cdf0ea29fe7443e38545e3292f3dc48e6192e8
Vivian <71410564+Viv-0@users.noreply.github.com>
2023-10-31 20:17:04 -0400

    Update src/Entities/StrawberryJam/MomentumBlock.cs
    Co-authored-by: catapillie <59423464+catapillie@users.noreply.github.com>

--- src/Entities/StrawberryJam/MomentumBlock.cs
+++ src/Entities/StrawberryJam/MomentumBlock.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Celeste.Mod.CommunalHelper.Entities.StrawberryJam;
+
 [CustomEntity("CommunalHelper/SJ/MomentumBlock")]
 public class MomentumBlock : Solid
 {
