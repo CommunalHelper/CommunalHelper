4922833f09ddda9e502bb5ffd402484b1afbde30
SnipUndercover <snipundercover@gmail.com>
2025-05-11 18:48:37 +0200

    Implement Dream Tunnel Blockers
    Allow the mapper to prevent entering a dream tunnel/dream dash
    when colliding with solids/dream blocks covered by this entity.

---
+++ Loenn/entities/misc/dream_tunnel_blocker.lua
@@ -0,0 +1,43 @@
+﻿local triggers = require("triggers")
+local depths = require("consts.object_depths")
+local drawableRectangle = require("structs.drawable_rectangle")
+local drawableText = require("structs.drawable_text")
+local colors = require("consts.colors")
+
+local dreamTunnelBlocker = {}
+
+dreamTunnelBlocker.name = "CommunalHelper/DreamTunnelBlocker"
+dreamTunnelBlocker.depth = depths.fakeWalls + 2000;
+
+dreamTunnelBlocker.placements = {
+    name = "normal",
+    data = {
+        width = 16,
+        height = 16,
+        blockDreamTunnelDashes = true,
+        blockDreamDashes = false
+    }
+}
+
+-- adapted from triggers.getDrawable
+function dreamTunnelBlocker.sprite(room, entity)
+    local displayName = triggers.triggerText(room, entity)
+
+    local x = entity.x or 0
+    local y = entity.y or 0
+
+    local width = entity.width or 16
+    local height = entity.height or 16
+
+    local borderedRectangle = drawableRectangle.fromRectangle("bordered", x, y, width, height, colors.triggerColor, colors.triggerBorderColor)
+    local textDrawable = drawableText.fromText(displayName, x, y, width, height, nil, triggers.triggerFontSize, colors.triggerTextColor)
+
+    local drawables = borderedRectangle:getDrawableSprite()
+    table.insert(drawables, textDrawable)
+
+    textDrawable.depth = dreamTunnelBlocker.depth - 1
+
+    return drawables
+end
+
+return dreamTunnelBlocker
---
+++ src/Entities/Misc/DreamTunnelBlocker.cs
@@ -0,0 +1,20 @@
+﻿namespace Celeste.Mod.CommunalHelper.Entities;
+
+[Tracked]
+[CustomEntity("CommunalHelper/DreamTunnelBlocker")]
+public class DreamTunnelBlocker : Entity
+{
+    public bool BlockDreamTunnelDashes;
+    public bool BlockDreamDashes;
+
+    /// Blocking behavior handled in <see cref="DashStates.DreamTunnelDash.DreamTunnelDashCheck" />
+    /// and <see cref="DashStates.DreamTunnelDash.Player_DreamDashCheck"/>
+    public DreamTunnelBlocker(EntityData data, Vector2 offset) : base(data.Position + offset)
+    {
+        Collider = new Hitbox(data.Width, data.Height);
+        Depth = Depths.FakeWalls + 2000;
+
+        BlockDreamTunnelDashes = data.Bool("blockDreamTunnelDashes", true);
+        BlockDreamDashes = data.Bool("blockDreamDashes", false);
+    }
+}
