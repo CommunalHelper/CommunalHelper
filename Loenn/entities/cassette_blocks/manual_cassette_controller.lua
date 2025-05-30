5830c27731844c4583cd8a8fbf506537cb4c519d
Etpio <EtpioCeleste@gmail.com>
2025-05-30 17:40:21 +0300

    Update manual_cassette_controller.lua
    Fixed the name and added flag data

--- Loenn/entities/cassette_blocks/manual_cassette_controller.lua
+++ Loenn/entities/cassette_blocks/manual_cassette_controller.lua
@@ -1,18 +1,22 @@
-local cassetteJumpFixController = {}
+local manualCassetteController = {}
 
-cassetteJumpFixController.name = "CommunalHelper/ManualCassetteController"
-cassetteJumpFixController.depth = -1000000
+manualCassetteController.name = "CommunalHelper/ManualCassetteController"
+manualCassetteController.depth = -1000000
 
-cassetteJumpFixController.placements = {
+manualCassetteController.placements = {
     {
         name = "controller",
         data = {
-            startIndex = 0
+            startIndex = 0,
+            blueFlag = "cas_blue",
+            pinkFlag = "cas_rose",
+            yellowFlag = "cas_brightsun",
+            greenFlag = "cas_malachite",
         }
     }
 }
 
 local alt = math.random(100) == 42
-cassetteJumpFixController.texture = string.format("objects/CommunalHelper/manualCassetteController/icon%s", (alt and "_wacked" or ""))
+manualCassetteController.texture = string.format("objects/CommunalHelper/manualCassetteController/icon%s", (alt and "_wacked" or ""))
 
-return cassetteJumpFixController
+return manualCassetteController
