65250503fa18f1bcd396574beea9507f7ac40ace
catapillie <59423464+catapillie@users.noreply.github.com>
2025-06-28 20:09:37 +0200

    Update Loenn/entities/misc/hint_controller.lua

--- Loenn/entities/misc/hint_controller.lua
+++ Loenn/entities/misc/hint_controller.lua
@@ -14,6 +14,7 @@ hintController.placements = {
             selectorCounter = "",
             selectNextHint = false,
 			flagWhileReading = ""
+            flagWhileReading = "",
         }
     }
 }
