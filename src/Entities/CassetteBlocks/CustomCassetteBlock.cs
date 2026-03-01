4759763f1170a87edfa92a66c4d6ce4ec18714f5
catapillie <59423464+catapillie@users.noreply.github.com>
2026-03-01 17:27:01 +0100

    Update src/Entities/CassetteBlocks/CustomCassetteBlock.cs

--- src/Entities/CassetteBlocks/CustomCassetteBlock.cs
+++ src/Entities/CassetteBlocks/CustomCassetteBlock.cs
@@ -3,7 +3,6 @@ using MonoMod.Utils;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using static MonoMod.InlineRT.MonoModRule;
 
 namespace Celeste.Mod.CommunalHelper.Entities;
 
