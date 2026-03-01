e2c79b50f2824f6e4b2acfe379547891d6e4b306
aonkeeper4 <aonkeeper4@gmail.com>
2026-03-01 23:35:00 +0000

    Finish merging dev i hate working from the commandline

--- Loenn/entities/cassette_blocks/custom_cassette_block.lua
+++ Loenn/entities/cassette_blocks/custom_cassette_block.lua
@@ -36,12 +36,9 @@ for i = 1, 4 do
             height = 16,
             customColor = colors[i],
             oldConnectionBehavior = false,
-<<<<<<< HEAD
             spritePath = "",
             sideAlpha = 1.0,
-=======
             held = false,
->>>>>>> 5ef928fe414af767303da25d68d3523b2af49bdf
         }
     }
 end
