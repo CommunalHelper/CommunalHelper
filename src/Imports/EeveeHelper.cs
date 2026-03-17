90ac7f888fd097ba926e22ed1fa64b1ce3bfa1d2
aonkeeper4 <aonkeeper4@gmail.com>
2026-03-17 02:47:33 +0000

    update to new implementation

--- src/Imports/EeveeHelper.cs
+++ src/Imports/EeveeHelper.cs
@@ -1,19 +1,21 @@
 using Celeste.Mod.CommunalHelper.Entities;
 using MonoMod.ModInterop;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Celeste.Mod.CommunalHelper.Imports;
 
 [ModImportName("EeveeHelper")]
 public static class EeveeHelper
 {
-    public static Action<Type, HashSet<string>> RegisterIgnoredAnchors;
+    public static Action<Type, bool, HashSet<string>> RegisterBlacklistedAnchors;
 
     public static void Initialize()
     {
         typeof(EeveeHelper).ModInterop();
 
-        RegisterIgnoredAnchors?.Invoke(typeof(ConnectedSolid), ["GroupBoundsMin", "GroupBoundsMax", "GroupOffset", "GroupCenter"]);
-        RegisterIgnoredAnchors?.Invoke(typeof(ConnectedDreamBlock), ["GroupBoundsMin", "GroupBoundsMax", "GroupOffset"]);
+        string[] defaultBlacklistedAnchors = ["Position", "ExactPosition", "TopLeft", "TopCenter", "TopRight", "Center", "CenterLeft", "CenterRight", "BottomLeft", "BottomCenter", "BottomRight"];
+        RegisterBlacklistedAnchors?.Invoke(typeof(ConnectedSolid), true, defaultBlacklistedAnchors.Union(["GroupBoundsMin", "GroupBoundsMax", "GroupOffset", "GroupCenter"]).ToHashSet());
+        RegisterBlacklistedAnchors?.Invoke(typeof(ConnectedDreamBlock), false, defaultBlacklistedAnchors.Union(["GroupBoundsMin", "GroupBoundsMax", "GroupOffset"]).ToHashSet());
     }
 }
